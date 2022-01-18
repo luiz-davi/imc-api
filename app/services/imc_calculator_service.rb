cf17081f8038e0acc0a19f7ed959a0d41c6e54c1
Luiz Davi <luizd4398@gmail.com>
2022-01-18 19:22:18 -0300

    teste de serviço finalizado

--- app/services/imc_calculator_service.rb
+++ app/services/imc_calculator_service.rb
@@ -1,16 +1,31 @@
 class ImcCalculatorService
     def self.calcular_imc(heigth, weigth)
-        imc = (weigth / (heigth*heigth)).floor(1)
-        classification = ""
-        obesity = ""
+        imc = weigth
+        imc /= heigth*heigth
+        imc = imc.ceil(1)
 
-        if imc < 16.5
-            classification = "Peso severamente abaixo do normal"
-        elsif imc >= 16.5 && imc < 18.5
-            classification = "Peso abaixo do normal"
+        if imc < 18.5
+            classification = "Magreza"
+            obesity = "0"
         elsif imc >= 18.5 && imc < 25
             classification = "Normal"
+            obesity = "0"
+        elsif imc >= 25 && imc < 30
+            classification = "Sobrepeso"
+            obesity = "I"
+        elsif imc >= 30 && imc < 40
+            classification = "Obesidade"
+            obesity = "II"
+        elsif imc >= 40
+            classification = "Obesidade grave"
+            obesity = "III"
+        end
 
-
+        {
+            "imc": imc,
+            "classification": classification,
+            "obesity": obesity
+        }
+        
     end
 end
--- spec/services/imc_calculator_service_spec.rb
+++ spec/services/imc_calculator_service_spec.rb
@@ -3,8 +3,12 @@ require 'rails_helper'
 describe ImcCalculatorService do
     describe 'imc' do
         it 'calcular imc' do
-            imc = described_class.calcular_imc(1.80,80)
-            expect(imc).to eq(24.6)
+            imc = described_class.calcular_imc(1.70,76)
+            expect(imc).to eq({
+                "imc": 26.3,
+                "classification": "Sobrepeso",
+                "obesity": "I" 
+            })
         end
     end
 end
